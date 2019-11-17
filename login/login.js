deb563a2306557622d51acbbeefa441894c97528
adir-avraham <adir.avraham@gmail.com>
2019-11-17 14:20:22 +0200

    all-noRedux

--- login/login.js
+++ login/login.js
@@ -2,18 +2,37 @@
 const users = require('../users.json')
 const express = require('express');
 const router = express.Router();
-
+const moment = require("moment");
+const usersKeys = {};
 
 router.use('/login', (req, res, next)=>{
 
     const {email, password} = req.body;
     const isLogin = users.find(user => user.email === email && user.password === password)
-    if (!isLogin) return res.json({message: "User name and/or password is incorrect"});
+    if (!isLogin) return res.json({message: "User name and/or password is incorrect", redirect: false});
     next();
 })
 
 router.post('/login', (req, res, next) =>{
-    res.json({message: "You are logged-in!!!", redirect: true})
+    const apiKey = getApiKey();
+    const expiration = moment().format("x");
+    usersKeys[apiKey] = expiration;
+    res.json({message: `You are logged-in!!! your key is ${apiKey}`, redirect: true, key: apiKey})
 })
+
+  
+  function getApiKey() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+      const r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
+  
+  
+
+
+
+
 
 module.exports = router;
